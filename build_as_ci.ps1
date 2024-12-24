b336937560e324c78bebaf78d2ce0ae1303e2a4c
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2024-12-24 15:41:17 -0800

    Fix incorrect string interpolation

--- build_as_ci.ps1
+++ build_as_ci.ps1
@@ -7,7 +7,7 @@ $env:ZLIB_LIBRARY = "$( Get-Location )\external\zlib\lib\zlibstatic.lib"
 $env:ZLIB_INCLUDE_DIR = "$( Get-Location )\external\zlib\include"
 
 Write-Output "[2/3] Configuring project..."
-cmake -DCMAKE_POLICY_DEFAULT_CMP0077=NEW -DCMAKE_PROGRAM_PATH=$(Get-Location) --fresh --preset ci-release -S . -B build/ci-release
+cmake -DCMAKE_POLICY_DEFAULT_CMP0077=NEW -DCMAKE_PROGRAM_PATH="$( Get-Location )" --fresh --preset ci-release -S . -B build/ci-release
 
 Write-Output "[3/3] Building project..."
 cmake --build build/ci-release -t gelly_installer
