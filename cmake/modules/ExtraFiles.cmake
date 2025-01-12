b4ab6b2d9fcf581abaad2967c890a23da1e30001
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2025-01-12 15:36:40 -0800

    Replace regular for medium

--- cmake/modules/ExtraFiles.cmake
+++ cmake/modules/ExtraFiles.cmake
@@ -1,6 +1,6 @@
 function(get_extra_files source_dir)
   set(EXTRA_FILES
-          ${source_dir}/fonts/Poppins-Regular.ttf
+          ${source_dir}/fonts/Poppins-Medium.ttf
           ${source_dir}/fonts/Poppins-Bold.ttf
           ${CURL_INCLUDE_DIRS}/../bin/libcurl-x64.dll
           ${CURL_INCLUDE_DIRS}/../bin/curl-ca-bundle.crt
